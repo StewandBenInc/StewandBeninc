5d2a2c5087e0e3708eb3ec13a9b2a8a7fd71e858
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-04-01 10:20:03 -0400

    added community page, still needs lots of work

---
+++ StudyGuides/community.js
@@ -0,0 +1,74 @@
+let headingCoonter = 1;
+let subheadingCoonter = [1];
+let itemCoonter = [[1]];
+function addHeading() {
+    let dataDiv = document.getElementById("headings");
+    ++headingCoonter;
+    subheadingCoonter.push(1);
+    itemCoonter.push([1]);
+    dataDiv.innerHTML += `<br>
+    <input type="text" id="heading${headingCoonter}" class="heading" required placeholder="Heading ${headingCoonter}">
+                <div id="subheadinglistheading${headingCoonter}">
+                    <input type="text" id="subheading1heading${headingCoonter}" class="subheading" required placeholder="Subheading 1 in Heading ${headingCoonter}">
+                    <br>
+                    <div id="itemlistsubheading1heading${headingCoonter}">
+                        <textarea id="item1subheading1heading" class="item" required placeholder="Item 1 in Subheading 1 in Heading ${headingCoonter}"></textarea>
+                        <br>
+                    </div>
+                    <button class="itemadder" onclick="addItem(${headingCoonter}, 1)">Add Item</button>
+                    <br>
+                </div>
+                <br>
+                <button class="subheadingadder" onclick="addSubheading(${headingCoonter})">Add Subheading</button>`;
+                console.log(itemCoonter);
+    return 0;
+}
+function addSubheading(heading) {
+    let dataDiv = document.getElementById(`subheadinglistheading${heading}`);
+    ++subheadingCoonter[heading-1];
+    itemCoonter[heading-1].push(1);
+    dataDiv.innerHTML += `<br>
+    <input type="text" id="subheading${subheadingCoonter[heading-1]}heading${heading}" class="subheading" required placeholder="Subheading ${subheadingCoonter[heading-1]} in Heading ${heading}">
+    <br>
+    <div id="itemlistsubheading${subheadingCoonter[heading-1]}heading${heading}">
+        <textarea id="item1subheading${subheadingCoonter[heading-1]}heading${heading}" class="item" required placeholder="Item 1 in Subheading ${subheadingCoonter[heading-1]} in Heading ${heading}"></textarea>
+        <br>
+    </div>
+    <button class="itemadder" onclick="addItem(${heading}, ${subheadingCoonter[heading-1]})">Add Item</button>`;
+    console.log(itemCoonter);
+    return 0;
+}
+function addItem(heading, subheading) {
+    let dataDiv = document.getElementById(`itemlistsubheading${subheading}heading${heading}`);
+    ++itemCoonter[heading-1][subheading-1];
+    dataDiv.innerHTML += `<textarea id="item${itemCoonter[heading-1][subheading-1]}subheading${subheading}heading${heading}" class="item" required placeholder="Item ${itemCoonter[heading-1][subheading-1]} in Subheading ${subheading} in Heading ${heading}"></textarea>
+    <br>`;
+    console.log(itemCoonter);
+    return 0;
+}
+function submit() {
+    let title = document.getElementById("title").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let headings = [];
+    let subheadings = [[]];
+    let items = [[[]]];
+    for (let i = 1; i <= headingCoonter; ++i) {
+        headings.push(document.getElementById(`heading${i}`).value);
+        for (let j = 1; j <= subheadingCoonter[i-1]; ++j) {
+            subheadings[i-1].push(document.getElementById(`subheading${j}heading${i}`).value);
+            for (let k = 1; k <= itemCoonter[i-1][j-1]; ++k) {
+                items[i-1][j-1].push(document.getElementById(`item${k}subheading${j}heading${i}`).value);
+            }
+        }
+    }
+    console.log(title);
+    console.log(name);
+    console.log(email);
+    console.log(subject);
+    console.log(headings);
+    console.log(subheadings);
+    console.log(items);
+    return 0;
+}
