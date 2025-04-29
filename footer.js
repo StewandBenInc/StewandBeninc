c61983a0af77764066fc816fabe99e0a94c8eae6
Ben Kiev <146984941+Bean91@users.noreply.github.com>
2025-04-29 11:08:42 -0400

    added things

--- footer.js
+++ footer.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(response => response.text())
       .then(data => {
         document.body.insertAdjacentHTML("beforeend", data);
+        const cookies = document.cookie.split(';');
+        let signedIn = false;
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.startsWith("username" + '=')) {
+                if(cookie.substring(9)) {
+                    signedIn = true;
+                }
+            }
+        }
+        if (!signedIn) {
+          document.getElementById("dash").style.display = "none";
+          document.getElementById("loginarea").style.display = "inline-block";
+        }
         const footer = document.querySelector(".footer");
         const body = document.body;
         const isContentShort = body.scrollHeight <= window.innerHeight;
