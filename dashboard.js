92269db97e33baf981a62f26227d42b05500c4c7
Ben Kiev <146984941+ItsCrazyWolfWasTaken@users.noreply.github.com>
2025-05-02 13:29:13 -0400

    asdf

--- dashboard.js
+++ dashboard.js
@@ -75,6 +75,7 @@ function signOut() {
 }
 
 async function deleteAccount() {
+    confirm("Are you sure you want to delete your account? This action cannot be undone.");
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
         let cookie = cookies[i].trim();
