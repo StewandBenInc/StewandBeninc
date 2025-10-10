03f07360f471e7ce4b9083a4a16df4bf4fb09530
HTML93 <stewartphippsgoodwin@gmail.com>
2025-10-10 17:45:14 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -7,6 +7,13 @@ function getCookie(name) {
     }
     return null;
 }
+function deleteAllCookies() {
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
+}
 const availArrayOps = [{
         name: "weather",
         signinReq: false
